a70de63b9b70e797fcd17f27c6ae328ed01ed84a
Dandy Nagel <github@dandynagel.com>
2025-11-29 04:57:41 +0100

    removed console.log

--- src/main.jsx
+++ src/main.jsx
@@ -7,9 +7,6 @@ import App from '@/App.jsx';
 
 const basename = import.meta.env.BASE_URL;
 
-const path = window.location;
-console.log(path);
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter basename={basename}>
