39de5b4cfdf775d94825994df7ac891fd11edd27
Dandy Nagel <github@dandynagel.com>
2025-11-29 05:51:16 +0100

    routing fix daisyui carousel to using buttons and js

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -56,18 +56,38 @@ const Home = () => {
             </div>
           </div>
           <div className='flex w-full justify-center gap-2 py-2'>
-            <a href='#item1' className='btn btn-xs'>
+            <button
+              onClick={() =>
+                document.getElementById('item1').scrollIntoView({ behavior: 'smooth' })
+              }
+              className='btn btn-xs'
+            >
               1
-            </a>
-            <a href='#item2' className='btn btn-xs'>
+            </button>
+            <button
+              onClick={() =>
+                document.getElementById('item2').scrollIntoView({ behavior: 'smooth' })
+              }
+              className='btn btn-xs'
+            >
               2
-            </a>
-            <a href='#item3' className='btn btn-xs'>
+            </button>
+            <button
+              onClick={() =>
+                document.getElementById('item3').scrollIntoView({ behavior: 'smooth' })
+              }
+              className='btn btn-xs'
+            >
               3
-            </a>
-            <a href='#item4' className='btn btn-xs'>
+            </button>
+            <button
+              onClick={() =>
+                document.getElementById('item4').scrollIntoView({ behavior: 'smooth' })
+              }
+              className='btn btn-xs'
+            >
               4
-            </a>
+            </button>
           </div>
         </figure>
       </div>
